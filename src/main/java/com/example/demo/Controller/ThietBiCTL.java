de5f1674ec350bab761b4abe2d9ca738a879f884
Duong Thanh Truong <truongnopro1111@gmail.com>
2024-05-04 22:17:55 +0700

    upload admin-thietbi

--- src/main/java/com/example/demo/Controller/ThietBiCTL.java
+++ src/main/java/com/example/demo/Controller/ThietBiCTL.java
@@ -1,16 +1,17 @@
 package com.example.demo.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 
-@RestController
+@Controller
 public class ThietBiCTL {
 
-	@GetMapping("/thietbi")
+	@GetMapping("/thiet-bi")
     public String getAllthietBi() {
         
-        return "thiết bị";
+        return "admin-thietbi";
     }
 }
